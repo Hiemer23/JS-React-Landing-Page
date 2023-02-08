3cd7e357279599e54810f25e6efa349456757157
Hiemer23 <andreyhiemer23@gmail.com>
2023-02-08 13:51:43 -0300

    Testes Heading

--- src/templates/App/App.test.jsx
+++ src/templates/App/App.test.jsx
@@ -3,9 +3,8 @@ import { screen } from '@testing-library/react'
 import Home from '.';
 import { theme } from '../../styles/theme';
 
-test('renders learn react link', () => {
-  const { debug } = renderTheme(<Home />);
-  const headingContainer = screen.getByRole('heading', { name: 'Hellow' })
-    .parentElement;
-  expect(headingContainer).toMatchSnapshot()
-});
+describe('<Home />', () => {
+  it('should render home', () => {
+    renderTheme(<Home />);
+  })
+})
